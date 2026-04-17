ce4cb3c154578338bf5cd4acc24c0ec515ea04f3
zhangpf1 <zhangpengfei@csc.com.cn>
2026-04-17 09:52:49 +0800

    fix: eslint

--- src/lib/config/axios/config.ts
+++ src/lib/config/axios/config.ts
@@ -2,17 +2,17 @@
  * axios全局配置
  * @Author: zhangpi
  */
-const defaultbaseURL = `${process.env.NEXT_PUBLIC_AXIOS_API_BASE_URL || 'http://localhost:3000'}${process.env.NEXT_PUBLIC_AXIOS_API_BASE_PATH || '/api/v1'}`
+const defaultbaseURL = `${process.env.NEXT_PUBLIC_AXIOS_API_BASE_PATH || '/api/v1'}`
 
 const config: {
-  result_code: number | string;
-  request_timeout: number;
-  defaultbaseURL: string;
+  result_code: number | string
+  request_timeout: number
+  defaultbaseURL: string
 } = {
   /**
    * 接口成功返回状态码
    */
-  result_code: "200",
+  result_code: '200',
   /**
    * 接口请求超时时间
    */
@@ -22,7 +22,7 @@ const config: {
    * 可选值: application/x-www-form-urlencoded multipart/form-data
    */
   // api前缀
-  defaultbaseURL,
-};
+  defaultbaseURL
+}
 
-export { config };
+export { config }
