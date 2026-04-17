595ae6f86f5f3d64002d1645086cacf166e6421c
zhangpf1 <zhangpengfei@csc.com.cn>
2026-04-17 15:51:50 +0800

    fix: build

--- next.config.ts
+++ next.config.ts
@@ -8,18 +8,6 @@ const nextConfig: NextConfig = {
     }
   },
   webpack: (config, { isServer }) => {
-    if (isServer) {
-      // 忽略服务器端不需要的浏览器专用模块
-      config.externals = [...(config.externals || []), 'pdf-parse']
-      // 提供 Node.js 中不存在的模块的空 polyfill
-      // config.resolve.fallback = {
-      //   ...config.resolve.fallback,
-      //   canvas: false,
-      //   jsdom: false,
-      //   bufferutil: false,
-      //   'utf-8-validate': false
-      // }
-    }
     return config
   }
 }
